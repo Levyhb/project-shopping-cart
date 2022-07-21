125785b12a1b555f66d01e8b0ce8671d1fa5afd4
Levy Bezerra <levybholanda@gmail.com>
2022-07-21 00:13:51 -0300

    Update fetchItem.js

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,14 +2,7 @@ const fetchItem = async (id) => {
   const url = `https://api.mercadolibre.com/items/${id}`;
   
   try {
-    const requisicao = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      body: 'param=',
-      headers: {
-        'Content-Type': 'application/json',
-  },
-});
+    const requisicao = await fetch(url);
     const response = await requisicao.json();
     return response;
   } catch (error) {
